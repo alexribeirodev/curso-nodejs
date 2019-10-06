8cbcc949a10355afefd0067c90771232ab0296c4
Alex Ribeiro <ribeiroa2000@gmail.com>
2019-10-06 14:30:20 -0300

    Build hugo

--- examples/expressjs/src/config.js
+++ examples/expressjs/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING || "mongodb+srv://cursonode:cursonode2019@cluster0-xixwd.mongodb.net/alex?retryWrites=true&w=majority"
+    MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING || "mongodb+srv://<username>:<password>@cluster0-xixwd.mongodb.net/<collection>?retryWrites=true&w=majority"
 }
